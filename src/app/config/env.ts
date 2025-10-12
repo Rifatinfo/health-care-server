96af532c2115e540f3b5198eded94195de992697
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-10-12 19:44:04 +0600

    add multer and cloludenary image upload

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -3,24 +3,30 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface EnvConfig {
-    PORT : string,
-    DATABASE_URL : string,
-    NODE_ENV : string
+    PORT: string,
+    DATABASE_URL: string,
+    NODE_ENV: string,
+    cloud_name: string,
+    api_key: string,
+    api_secret: string
 }
 
-const loadEnvVariable = () : EnvConfig => {
-    const requiredEnvVariable : string[] = ["PORT", "DATABASE_URL", "NODE_ENV"]
+const loadEnvVariable = (): EnvConfig => {
+    const requiredEnvVariable: string[] = ["PORT", "DATABASE_URL", "NODE_ENV", "cloud_name", "api_key", "api_secret"]
 
     requiredEnvVariable.forEach(key => {
-        if(!process.env[key]){
-           throw new Error(`Missing required environment variable ${key}`)
+        if (!process.env[key]) {
+            throw new Error(`Missing required environment variable ${key}`)
         }
     })
 
     return {
-        PORT : process.env.PORT as string,
-        DATABASE_URL : process.env.DATABASE_URL as string,
-        NODE_ENV : process.env.NODE_ENV as string
+        PORT: process.env.PORT as string,
+        DATABASE_URL: process.env.DATABASE_URL as string,
+        NODE_ENV: process.env.NODE_ENV as string,
+        cloud_name : process.env.cloud_name as string,
+        api_key : process.env.api_key as string,
+        api_secret : process.env.api_secret as string
     }
 }
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,30 @@
+import { Request } from "express";
 import { prisma } from "../../config/db";
-import { createPatientInput } from "./user.interface";
 import bcrypt from "bcryptjs";
+import { fileUploader } from "../../helper/fileUploader";
 
-const createPatient = async (payload : createPatientInput) => {
-    const hashPassword = await bcrypt.hash(payload.password, 10);
+const createPatient = async (req : Request) => {
+    if(req.file){
+       const uploadResult = await fileUploader.uploadToCloudinary(req.file);
+       console.log(uploadResult);
+       
+    }
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
 
     const result = await prisma.$transaction(async (tnx) => {
          await tnx.user.create({
             data : {
-                email : payload.email,
+                email : req.body.patient.email,
                 password : hashPassword
             }
         })
 
         return await tnx.patient.create({
-            data : {
-                name : payload.name,
-                email : payload.email
-            }
+            // data : {
+            //     name : req.body.name,
+            //     email : req.body.email
+            // }
+            data : req.body.patient
         })
     })
 
