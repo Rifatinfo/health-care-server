86204112517ca66a53941a75308b620dc998c788
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-10-15 11:55:36 +0600

    add auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../errorHelpers/AppError";
 import { StatusCodes } from "http-status-codes";
+import { jwtHelper } from "../helper/jwthelper";
 
 const auth = (...roles : string[]) => {
    return async (req : Request & {user?:any} , res : Response, next : NextFunction) => {
@@ -10,6 +11,14 @@ const auth = (...roles : string[]) => {
           if(!token){
                throw new AppError(StatusCodes.BAD_REQUEST, "You are not authorized!!!");
           }
+          const verifyUser = jwtHelper.verifyToken(token, "abcd");
+          req.user = verifyUser;
+
+          if(roles.length && !roles.includes(verifyUser.role)){
+             throw new AppError(StatusCodes.BAD_REQUEST, "You are not authorized");
+          }
+
+          next();
           
       } catch(err){
           next();
