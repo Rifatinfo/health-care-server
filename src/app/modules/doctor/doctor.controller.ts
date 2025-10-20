d104ed5c8386b6a12d70f0f89e317736c170dff6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-10-20 19:26:59 +0600

    add doctor service

---
+++ src/app/modules/doctor/doctor.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../middlewares/catchAsync";
+import { sendResponse } from "../../middlewares/sendResponse";
+import pick from "../../helper/pick";
+import { doctorFilterableFields } from "./doctor.constant";
+import { DoctorService } from "./doctor.service";
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const filters = pick(req.query, doctorFilterableFields)
+
+    const result = await DoctorService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Doctor fetched Successfully!",
+        meta: result.meta,
+        data: result.data,
+    })
+})
+
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorService.updateIntoDB(id, req.body);
+    
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Doctor fetched Successfully!",
+        data: result,
+    })
+})
+
+export const DoctorController = {
+    getAllFromDB,
+    updateIntoDB
+}
